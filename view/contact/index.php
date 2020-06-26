81e848f033f8917f7fa2ad0fa4a7faba3749f4ca
Bartek <99047458@mydavinci.nl>
2020-06-26 11:17:44 +0200

    contact gefixed.

--- view/contact/index.php
+++ view/contact/index.php
@@ -1,19 +1,31 @@
 
 <div container>
     <div class="col-12">
-          <h3 class="text-center">Heeft u problemen met uw auto/reservering?</h3><br>
-          <p class="text-center pb-3">Dan kunt u contact opnemen via het volgende:</p>
+          <h3 class="text-center">Neem contact met ons op</h3><br>
+          <p class="text-center">Bij Carrental bent u bij het goede adres voor het huren van een auto. Voor vragen en meer informatie over Carrental kunt u met onze servicemedewerkers contact opnemen.</p>
+          <p class="text-center">Tijden:</p>
+          <p class="text-center">Maandag: van 08:00 tot 12:00</p>
+          <p class="text-center">Dinsdag: van 8:00 tot 17:00</p>
+          <p class="text-center">Woensdag: van 8:00 tot 17:00</p>
+          <p class="text-center">Donderdag: van 8:00 tot 17:00</p>
+          <p class="text-center">Vrijdag: van 8:00 tot 17:00</p>
+          <p class="text-center pb-2">Zaterdag en Zondag: gesloten</p>
+          <p class="text-center pb-5">U kunt via de mail op elk moment vragen stellen en informatie krijgen. We proberen binnen 48 uur te reageren.</p>
     </div>
 
 
-      <div class="col-6">
-            <p>Voor problemen met uw auto kunt u bellen naar:</p>
-        <div class="icons row">
+      <div class="text-center">
+            <p>Voor problemen met uw auto kunt u contact opnemen via:</p>
+        <div class="text-center font-weight-bold">
             <i class="fas fa-phone-alt"></i><p>+31 0612345678</p>
         </div>
-          <p>Voor problemen met uw reservering kunt u bellen naar:</p>
-        <div class="icons row">
+          <p>Voor problemen met uw reservering kunt u contact opnemen via:</p>
+        <div class="text-center font-weight-bold">
             <i class="fas fa-phone-alt"></i><p>+31 0687654321</p>
+        </div>
+          <p>We zijn ook berijkbaar via:</p>
+        <div class="text-center font-weight-bold">
+            <i class="fas fa-at"></i><p>contact@carrental.nl</p>
         </div>
       </div>
 
