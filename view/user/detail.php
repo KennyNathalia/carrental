96ed670d3161e4b0a27918ec45a206ea62e45cac
Bartek <99047458@mydavinci.nl>
2020-06-26 13:58:26 +0200

    user detail pagina layout

--- view/user/detail.php
+++ view/user/detail.php
@@ -2,3 +2,19 @@
 <h2 class="color-blue text-center m-3">Account</h2>
 
 <a class="btn btn-outline-danger d-block m-auto col-1" href="<?= URL ?>user/logout">Uitloggen</a>
+
+<div class="container-fluid">
+    <div class="text-center">  
+      
+          <div class="text-center">
+
+          	<p class="color-green"><i class="fas fa-user fa-10x"></i></p>
+
+              <p class="m-5">
+                <a href="#" class="btn btn-outline-success m-auto">Account wijzigen</a>
+                <a href="#" class="btn btn-outline-danger m-auto">Account verwijderen</a>                
+              </p>
+          </div>
+     
+    </div> 
+  </div> 
