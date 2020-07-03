a027cfa6af6aab9bc885b8e5a236fc15f44549a3
RickSnijders <ricksnijders01@outlook.com>
2020-07-03 09:58:49 +0200

    Responsive gemaakt

--- view/car/index.php
+++ view/car/index.php
@@ -10,11 +10,11 @@ $_SESSION['error'] = "";
 <h1 class="ml-5 color-blue">Auto lijst</h1>
 <?php foreach($cars as $row){?>
 	<div class="m-1 row border">
-		<section class="col-6">
+		<section class="col-lg-6 col-sm-12">
 		<h3 class="font-weight-bold m-3 col-12 color-blue"><?php echo $row['merknaam'] ?> <?php echo $row['modelnaam'] ?></h3>
-		<img class="col-7 m-3" src="<?=URL?>public/Auto-images/<?php echo $row['img'] ?>">
+		<img class="col-lg-7 col-sm-12 m-lg-3" src="<?=URL?>public/Auto-images/<?php echo $row['img'] ?>">
 		</section>
-		<section class="col-3 mb-5">
+		<section class="col-lg-3 col-sm-12 mb-5">
 			<h2 class="mt-5 mb-3">€<?php $number= $row['prijs']; echo number_format($number, 0, ',','.'); ?>,-</h2>
 			<p>Huurprijs (per dag): €<?php $huur = $row['prijs'] * 0.002; echo number_format($huur, 0, ',','.');?>,- </p>
 			
